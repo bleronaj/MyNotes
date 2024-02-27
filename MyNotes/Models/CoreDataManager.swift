c0d7b8982c8c4b3b32e1071959b6a2f9cd4099f0
blinaretkoceri <blina.retkoceri@student.uni-pr.edu>
2024-02-27 03:28:48 -0800

    Created func load

--- MyNotes/Models/CoreDataManager.swift
+++ MyNotes/Models/CoreDataManager.swift
@@ -10,11 +10,23 @@ import CoreData
 
 class CoreDataManager{
     
-    static let shared = CoreDataManager( modelName: "MyNotes")
-    
-    init(modelName: String) {
-        <#statements#>
-    }
-    
-    
-}
+   
+    static let shared = CoreDataManager(modelName: "MyNotes")
+     
+      let persistentContainer: NSPersistentContainer
+      var viewContext: NSManagedObjectContext {
+          return persistentContainer.viewContext
+      }
+     
+      init(modelName: String) {
+          persistentContainer = NSPersistentContainer(name: modelName)
+      }
+     
+      func load(completion: (() -> Void)? = nil) {
+          persistentContainer.loadPersistentStores { (description, error) in
+              guard error == nil else {
+                  fatalError(error!.localizedDescription)
+              }
+              completion?()
+          }
+      }}
