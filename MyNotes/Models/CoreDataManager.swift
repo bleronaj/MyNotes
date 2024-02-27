576faf2ae577559902bc1f4e7206eb3ad9848a57
Blerona <bleronaj>
2024-02-27 03:16:01 -0800

    Creating our CoreDataManager

---
+++ MyNotes/Models/CoreDataManager.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataManager.swift
+//  MyNotes
+//
+//  Created by blerona on 27.2.24.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager{
+    
+    static let shared = CoreDataManager( modelName: "MyNotes")
+    
+    init(modelName: String) {
+        <#statements#>
+    }
+    
+    
+}
