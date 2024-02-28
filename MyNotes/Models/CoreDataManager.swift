6b06da5b11860f239f35081287dd01f7f8206c19
blinaretkoceri <blina.retkoceri@student.uni-pr.edu>
2024-02-28 11:55:57 -0800

    Added helper functions to CoreDataManager for creating, fetching and deleting notes.

--- MyNotes/Models/CoreDataManager.swift
+++ MyNotes/Models/CoreDataManager.swift
@@ -35,5 +35,32 @@ class CoreDataManager {
                 print("An error ocurred while saving: \(error.localizedDescription)")
             }
         }
+    }extension CoreDataManager {
+        func createNote() -> Note {
+            let note = Note(context: viewContext)
+            note.id = UUID()
+            note.text = ""
+            note.lastUpdated = Date()
+            save()
+            return note
+        }
+       
+        func fetchNotes(filter: String? = nil) -> [Note] {
+            let request: NSFetchRequest<Note> = Note.fetchRequest()
+            let sortDescriptor = NSSortDescriptor(keyPath: \Note.lastUpdated, ascending: false)
+            request.sortDescriptors = [sortDescriptor]
+           
+            if let filter = filter {
+                let predicate = NSPredicate(format: "text contains[cd] %@", filter)
+                request.predicate = predicate
+            }
+           
+            return (try? viewContext.fetch(request)) ?? []
+        }
+       
+        func deleteNote(_ note: Note) {
+            viewContext.delete(note)
+            save()
+        }
+        
     }
-}
